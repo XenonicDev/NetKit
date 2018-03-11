e8fcddff261e051d33edbc9eabe47947f9b2a00b
XenonicDev <andrewdepke@gmail.com>
2018-03-10 18:48:38 -0700

    Added Socket Creation, Binding, and Sending

---
+++ NetKit/SocketUtils.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "Platform.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int CreateSocket(int Protocol)
+{
+	int Result = socket(AF_INET, SOCK_RAW, Protocol);
+	if (Result == -1)
+	{
+		printf("CreateSocket Failed\n");
+
+		return -1;
+	}
+
+	bool Option = true;
+
+	if (setsockopt(Result, IPPROTO_IP, IP_HDRINCL, (char*)&Option, sizeof(Option)) != 0)
+	{
+		printf("CreateSocket Failed at Setting Custom Headers\n");
+
+		return Result;
+	}
+
+	return Result;
+}
+
+int BindSocket(int Socket, unsigned long Address, int Port)
+{
+	struct sockaddr_in Data;
+	Data.sin_family = AF_INET;
+	Data.sin_addr.s_addr = Address;
+	Data.sin_port = htons(Port);
+
+	return bind(Socket, (struct sockaddr*)&Data, sizeof(Data));
+}
+
+int SendPacket(int Socket, unsigned char* Buffer, size_t BufferLength)
+{
+	int Sent = send(Socket, (char*)Buffer, BufferLength, 0);
+
+	if (Sent != BufferLength)
+	{
+		printf("SendPacket Warning: Total Bytes Sent Did Not Match Size of Packet Buffer. Total Bytes Sent: %d\n", Sent);
+
+		return 1;
+	}
+
+	return 0;
+}
--- NetKit/TCPHeader.h
+++ NetKit/TCPHeader.h
@@ -11,16 +11,16 @@ struct TCP_HEADER
 	unsigned short dest;  // Destination Port
 	unsigned int seq;  // Sequence Number
 	unsigned int ack_seq;  // Acknowledgment Number
-	unsigned short res1 : 4;
-	unsigned short doff : 4;
-	unsigned short fin : 1;
-	unsigned short syn : 1;
-	unsigned short rst : 1;
-	unsigned short psh : 1;
-	unsigned short ack : 1;
-	unsigned short urg : 1;
-	unsigned short ece : 1;
-	unsigned short cwr : 1;
+	unsigned short res1 : 4;  // Reserved Part 1
+	unsigned short doff : 4;  // Data Offset
+	unsigned short fin : 1;  // Finish Flag
+	unsigned short syn : 1;  // Synchronize Flag
+	unsigned short rst : 1;  // Reset Flag
+	unsigned short psh : 1;  // Push Flag
+	unsigned short ack : 1;  // Acknowledgment Flag
+	unsigned short urg : 1;  // Urgent Flag
+	unsigned short ece : 1;  // ECN-Echo Flag
+	unsigned short cwr : 1;  // Congestion Window Reduced Flag
 	unsigned short window;  // Window Size
 	unsigned short check;  // Checksum
 	unsigned short urg_ptr;  // Urgent Pointer
